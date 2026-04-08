a8bbe7b57d86bd9b8c8dd0a12a30db586455ad15
salim jadhaw <140444228+salim-jadhaw@users.noreply.github.com>
2026-04-08 15:05:05 +0530

    Update MainActivity.kt
    UI (User Interface ) Updated

--- app/src/main/java/sa/example/paisabazaar/MainActivity.kt
+++ app/src/main/java/sa/example/paisabazaar/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import sa.example.paisabazaar.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
